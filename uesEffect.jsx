5093fe245a470b7ec5513b7aa65e82464e81dce1
Virender Singh <109612471+virbaba@users.noreply.github.com>
2023-12-04 17:17:04 +0530

    Update uesEffect.jsx

--- uesEffect.jsx
+++ uesEffect.jsx
@@ -1,25 +1,60 @@
-import { useEffect } from "react";
-import { useState } from "react";
+/*
+  SPINNER.JSX
+*/
 
-function App() {
-  let [date, setDate] = useState(new Date());
-
-  useEffect(() => {
-    let intervalId = setInterval(() => {
-      setDate(new Date());
-    },1000);
-
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, []);
+function Spinner() {
+    
+ 
   return (
     <>
-      <center>
-        <h2>
-          {date.toLocaleString()}
-        </h2>
-      </center>
+      <div className="d-flex justify-content-center">
+        <div className="spinner-border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Spinner;
+
+/* APP.JSX */
+import { useEffect } from "react";
+import { useState } from "react";
+import Spinner from "./Spinner";
+
+function App() {
+  const [fetchData, setFetchData] = useState(false);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    setFetchData(true);
+
+    fetch("https://dummyjson.com/products")
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data.products); // Set the fetched data to the state variable
+        setFetchData(false); // Update fetchData state to false after data is fetched
+      })
+
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setFetchData(false); // Update fetchData state to false in case of an error
+      });
+  }, []);
+
+  return (
+    <>
+      {fetchData ? (
+        <Spinner />
+      ) : (
+        <div>
+          {/* Render your products or use the data fetched */}
+          {products.map((product) => (
+            <div key={product.id}>{product.title}hi</div>
+          ))}
+        </div>
+      )}
     </>
   );
 }
